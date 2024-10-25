fd43add4473fc09efc19dc70a2a7c8d45b7006f7
Dr. Németh Tamás <tomuwhu@gmail.com>
2024-10-25 19:17:53 +0200

    .

--- src/App.svelte
+++ src/App.svelte
@@ -19,13 +19,18 @@
     {#each data as elem}
     <tr>
       <td>{elem.id}</td>
-      <td class="pointer" on:click={() => document.getElementById(`h${elem.id}`).classList.toggle('hidden')}>{elem.title}</td>
+      <td class="pointer" on:click={() => {
+        document.getElementById(`h${elem.id}`).classList.toggle('hidden')
+        document.getElementById(`d${elem.id}`).classList.toggle('hidden')
+      }}>{elem.title}</td>
       <td>{elem.price}</td>
       <td>{elem.category}</td>
     </tr>
-    <tr class="hidden" id="h{elem.id}">
-      <td colspan="6">{elem.description}</td>
-      <td><img src="{elem.image}" alt="{elem.title}"></td>
+    <tr class="hidden hx" id="h{elem.id}">
+      <td colspan="4"><img src="{elem.image}" alt="{elem.title}"></td>
+    </tr>
+    <tr class="hidden hx" id="d{elem.id}">
+      <td colspan="4">{elem.description}</td>
     </tr>
     {/each}
   </table>
@@ -37,12 +42,20 @@ table {
  font-size: 12px;
 }
 img {
-  width: 300px;
+  height: 400px;
+  max-width: 500px;
+  border:solid 10px white;
+  border-radius: 10px;
+  box-shadow: 1px 1px 5px black;
 }
 tr.hidden {
   display: none;
 }
 td.pointer {
   cursor: pointer;
+}
+tr.hx td {
+  background-color: antiquewhite;
+  color: black;
 }
 </style>
